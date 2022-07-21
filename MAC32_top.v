4780be4677240228186f4a1cdf38a807a7397ce7
hankshyu <hankshyu@gmail.com>
2022-07-21 21:07:19 +0800

    finish sp detection

--- MAC32_top.v
+++ MAC32_top.v
@@ -80,31 +80,65 @@ module MAC32_top #(
     parameter PARM_RM_RTZ       = 3'd1;
     parameter PARM_RM_RDN       = 3'd2;
     parameter PARM_RM_RUP       = 3'd3;
-    // parameter PARM_PC            = 5;
+    parameter PARM_PC            = 5; //?
     parameter PARM_EXP          = 8;
     parameter PARM_MANT         = 23;
-    // parameter PARM_MANT_PRENORM  = 24;
+    parameter PARM_MANT_PRENORM  = 24; //?
     parameter PARM_BIAS         = 127;
-    //parameter PARM_HALF_BIAS     = 63;
-    //parameter PARM_LEADONE_WIDTH = 7,
+    parameter PARM_HALF_BIAS     = 63;
+    parameter PARM_LEADONE_WIDTH = 7; //?
     parameter PARM_EXP_ZERO     = 8'h00;
-    parameter PARM_EXP_ONE      = 8'h01;
-    parameter PARM_EXP_INF      = 8'hff;
-    parameter PARM_MANT_ZERO    = 23'd0;
-   //RISC-V defines canonical NaN to be 0x7fc0_0000
-    parameter PARM_MANT_NAN     = 23'b100_0000_0000_0000_0000_0000;
+    parameter PARM_EXP_ONE      = 8'h01; //used in this
+    parameter PARM_EXP_INF      = 8'hff; //used in SpecialCaseDetector
+    parameter PARM_MANT_NAN     = 23'b100_0000_0000_0000_0000_0000; //RISC-V defines canonical NaN to be 0x7fc0_0000
+    parameter PARM_MANT_ZERO    = 23'd0; //used in SpecialCaseDetector
 
-    wire Sign_a, Sign_b, Sign_c;
-    wire [PARM_EXP - 1 : 0] Exp_a;
-    wire [PARM_EXP - 1 : 0] Exp_b;
-    wire [PARM_EXP - 1 : 0] Exp_c;
-    wire [PARM_MANT - 1 : 0] Mant_a;
-    wire [PARM_MANT - 1 : 0] Mant_b;
-    wire [PARM_MANT - 1 : 0] Mant_c;
 
-    assign {Sign_a, Exp_a, Mant_a} = A_i;
-    assign {Sign_b, Exp_b, Mant_b} = B_i;
-    assign {Sign_c, Exp_c, Mant_c} = C_i;
+    //inputs wires of specialCaseDetectors
+    wire A_Leadingbit = | A_i[PARM_XLEN - 2 : PARM_MANT]; //normalized number has leading 1, denormalized with leading 0
+    wire B_Leadingbit = | B_i[PARM_XLEN - 2 : PARM_MANT];
+    wire C_Leadingbit = | C_i[PARM_XLEN - 2 : PARM_MANT];
+    //outputs wires of specialCaseDetectors
+    wire A_Inf, B_Inf, C_Inf;
+    wire A_Zero, B_Zero, C_Zero;
+    wire A_NaN, B_NaN, C_NaN;
+    wire A_DeN, B_DeN, C_DeN;
+
+    SpecialCaseDetector specialCaseDetector(
+        .A_i(A_i),
+        .B_i(B_i),
+        .C_i(C_i),
+        .A_Leadingbit(A_Leadingbit),
+        .B_Leadingbit(B_Leadingbit),
+        .C_Leadingbit(C_Leadingbit),
+        .A_Inf_o(A_Inf),
+        .B_Inf_o(B_Inf),
+        .C_Inf_o(C_Inf),
+        .A_Zero_o(A_Zero),
+        .B_Zero_o(B_Zero),
+        .C_Zero_o(C_Zero),
+        .A_NaN_o(A_NaN),
+        .B_NaN_o(B_NaN),
+        .C_NaN_o(C_NaN),
+        .A_DeN_o(A_DeN),
+        .B_DeN_o(B_DeN),
+        .C_DeN_o(C_DeN)
+    );
+
+
+    wire A_Sign = A_i[PARM_XLEN - 1];
+    wire B_Sign = B_i[PARM_XLEN - 1];
+    wire C_Sign = C_i[PARM_XLEN - 1];
+    
+    //denormalized number has exponent 1 
+    wire [PARM_EXP - 1: 0] A_Exp = A_DeN? PARM_EXP_ONE : A_i[PARM_XLEN - 2 : PARM_MANT];
+    wire [PARM_EXP - 1: 0] B_Exp = B_DeN? PARM_EXP_ONE : B_i[PARM_XLEN - 2 : PARM_MANT];
+    wire [PARM_EXP - 1: 0] C_Exp = C_DeN? PARM_EXP_ONE : B_i[PARM_XLEN - 2 : PARM_MANT];
+    
+    wire [PARM_MANT : 0] A_Mant = {A_Leadingbit, A_i[PARM_MANT - 1 : 0]};
+    wire [PARM_MANT : 0] B_Mant = {B_Leadingbit, B_i[PARM_MANT - 1 : 0]};
+    wire [PARM_MANT : 0] C_Mant = {C_Leadingbit, C_i[PARM_MANT - 1 : 0]};
+
 
 
 
--- SpecialCaseDetector.v
+++ SpecialCaseDetector.v
@@ -23,42 +23,62 @@
 module SpecialCaseDetector #(
     parameter PARM_XLEN     = 32,
     parameter PARM_EXP      = 8,
-    parameter PARM_MANT     = 23,
-    parameter PARM_EXP_ONE  = 8'h01
+    parameter PARM_MANT     = 23
 ) (
     input [PARM_XLEN - 1 : 0] A_i,
     input [PARM_XLEN - 1 : 0] B_i,
     input [PARM_XLEN - 1 : 0] C_i,
+    input A_Leadingbit,
+    input B_Leadingbit,
+    input C_Leadingbit,
     
     output A_Inf_o,
-    output A_Zero_o,
-    output A_NaN_o,
-    output A_DeN_o,
     output B_Inf_o,
-    output B_Zero_o,
-    output B_NaN_o,
-    output B_DeN_o,
     output C_Inf_o,
+    output A_Zero_o,
+    output B_Zero_o,
     output C_Zero_o,
+    output A_NaN_o,
+    output B_NaN_o,
     output C_NaN_o,
+    output A_DeN_o,
+    output B_DeN_o,
     output C_DeN_o);
 
-    wire A_Leadingbit = A_i[PARM_XLEN - 2 : PARM_MANT]; //leading 1, if not exp field == 0
-    wire B_Leadingbit = B_i[PARM_XLEN - 2 : PARM_MANT];
-    wire C_Leadingbit = C_i[PARM_XLEN - 2 : PARM_MANT];
 
-    wire A_Sign = A_i[PARM_XLEN - 1];
-    wire B_Sign = B_i[PARM_XLEN - 1];
-    wire C_Sign = C_i[PARM_XLEN - 1];
-    wire [PARM_EXP - 1: 0] A_Exp = A_DeN_o? PARM_EXP_ONE : A_i[PARM_XLEN - 2 : PARM_MANT];
-    wire [PARM_EXP - 1: 0] B_Exp = B_DeN_o? PARM_EXP_ONE : B_i[PARM_XLEN - 2 : PARM_MANT];
-    wire [PARM_EXP - 1: 0] C_Exp = C_DeN_o? PARM_EXP_ONE : B_i[PARM_XLEN - 2 : PARM_MANT];
-    wire [PARM_MANT : 0] A_Mant;
-    wire [PARM_MANT : 0] B_Mant;
-    wire [PARM_MANT : 0] C_Mant;
+    parameter PARM_EXP_FULL     = 8'hff;
+    parameter PARM_MANT_ZERO    = 23'd0;
+
+    
+    wire A_ExpZero = ~A_Leadingbit;
+    wire B_ExpZero = ~B_Leadingbit;
+    wire C_ExpZero = ~C_Leadingbit;
+
+    wire A_ExpFull = (A_i[PARM_XLEN - 2 : PARM_MANT] == PARM_EXP_FULL);
+    wire B_ExpFull = (B_i[PARM_XLEN - 2 : PARM_MANT] == PARM_EXP_FULL);
+    wire C_ExpFull = (C_i[PARM_XLEN - 2 : PARM_MANT] == PARM_EXP_FULL);
+
+    wire A_MantZero = (A_i[PARM_MANT - 1 : 0] == PARM_MANT_ZERO);
+    wire B_MantZero = (B_i[PARM_MANT - 1 : 0] == PARM_MANT_ZERO);
+    wire C_MantZero = (C_i[PARM_MANT - 1 : 0] == PARM_MANT_ZERO);
 
 
 
+    assign A_Zero_o = A_ExpZero & A_MantZero;
+    assign B_Zero_o = B_ExpZero & B_MantZero;
+    assign C_Zero_o = C_ExpZero & C_MantZero;
+
+    assign A_Inf_o = A_ExpFull & A_MantZero;
+    assign B_Inf_o = B_ExpFull & B_MantZero;
+    assign C_Inf_o = C_ExpFull & C_MantZero;
+
+    assign A_NaN_o = A_ExpFull & (~A_MantZero);
+    assign B_NaN_o = B_ExpFull & (~B_MantZero);
+    assign C_NaN_o = C_ExpFull & (~C_MantZero);
+
+    assign A_DeN_o  = A_ExpZero & (~A_MantZero);
+    assign B_DeN_o  = B_ExpZero & (~B_MantZero);
+    assign C_DeN_o  = C_ExpZero & (~C_MantZero);
 
     
 endmodule
