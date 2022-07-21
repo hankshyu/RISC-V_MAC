9f47f991b5347501a09a07f2a8887be0a866be39
hankshyu <hankshyu@gmail.com>
2022-07-21 19:42:20 +0800

    special case detection

---
+++ SpecialCaseDetector.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/21/2022 07:41:57 PM
+// Design Name: 
+// Module Name: SpecialCaseDetector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SpecialCaseDetector(
+
+    );
+endmodule
