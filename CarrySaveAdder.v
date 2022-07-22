7dd30b21896a8d5089abe2c8e7546b8de7f57e06
hankshyu <hankshyu@gmail.com>
2022-07-22 10:23:16 +0800

    csa finish

--- CarrySaveAdder.v
+++ CarrySaveAdder.v
@@ -20,11 +20,6 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module CarrySaveAdder(
-    
-    );
-endmodule
-
 module CarrySaveAdder #(
     parameter XLEN = 49
 ) (
@@ -32,9 +27,21 @@ module CarrySaveAdder #(
     input [XLEN - 1 : 0] B_i,
     input [XLEN - 1 : 0] C_i,
     output [XLEN - 1 : 0] Sum_o,
-    output [XLEN - 1 : 0] Cy_o,
-
+    output [XLEN - 1 : 0] Cy_o
 );
-    
+    generate
+        genvar j;
+        for(j = 0; j < XLEN; j = j+1)begin
+            FullAdder FA(
+                .augend_i(A_i[j]),
+                .addend_i(B_i[j]),
+                .carry_i(C_i[j]),
+                .sum_o(Sum_o[j]),
+                .carry_o(Cy_o[j])
+            );
+            
+        end
+    endgenerate
+
     
 endmodule
