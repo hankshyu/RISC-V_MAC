84e8de5edf47534741302988765bf430bc591f9c
hankshyu <hankshyu@gmail.com>
2022-07-22 10:17:47 +0800

    adders file created

---
+++ CarrySaveAdder.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/22/2022 10:09:16 AM
+// Design Name: 
+// Module Name: CarrySaveAdder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CarrySaveAdder(
+    
+    );
+endmodule
+
+module CarrySaveAdder #(
+    parameter XLEN = 49
+) (
+    input [XLEN - 1 : 0] A_i,
+    input [XLEN - 1 : 0] B_i,
+    input [XLEN - 1 : 0] C_i,
+    output [XLEN - 1 : 0] Sum_o,
+    output [XLEN - 1 : 0] Cy_o,
+
+);
+    
+    
+endmodule
---
+++ FullAdder.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/22/2022 10:13:32 AM
+// Design Name: 
+// Module Name: FullAdder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FullAdder(
+    input augend_i,
+    input addend_i,
+    input carry_i,
+    output sum_o,
+    output carry_o);
+
+    assign Sum_o = augend_i ^ addend_i ^ carry_i;
+    assign carry_i = (augend_i & addend_i) || (addend_i & carry_i) || (carry_i & augend_i);
+endmodule
