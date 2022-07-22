4a1e1ef6a2e0789d4f0902dc4e1a775d9e9393ab
hankshyu <hankshyu@gmail.com>
2022-07-22 10:59:33 +0800

    booth top added

---
+++ R4Booth.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/22/2022 10:59:09 AM
+// Design Name: 
+// Module Name: R4Booth
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module R4Booth(
+
+    );
+endmodule
--- R4Booth_BitPairRecorder.v
+++ R4Booth_BitPairRecorder.v
@@ -21,6 +21,25 @@
 
 
 module R4Booth_BitPairRecorder(
+    input [2:0] pattern_i,
+    output mul1x_o,
+    output mul2x_o,
+    output mulsign_o);
 
-    );
+    //Modified Booth's Recording Table
+    // Multiplier   
+    //| Bit i + 1   |   Bit i   |   Bit i - 1   |   Multiplicand selected   |
+    //|     0       |    0      |       0       |   0 x Multiplicand        |
+    //|     0       |    0      |       1       |  +1 x Multiplicand        |
+    //|     0       |    1      |       0       |  +1 x Multiplicand        |
+    //|     0       |    1      |       1       |  +2 x Multiplicand        |
+    //|     1       |    0      |       0       |  -2 x Multiplicand        |
+    //|     1       |    0      |       1       |  -1 x Multiplicand        |
+    //|     1       |    1      |       0       |  -1 x Multiplicand        |
+    //|     1       |    1      |       1       |   0 x Multiplicand        |
+    
+    assign mul1x_o = (pattern_i[1] ^ pattern_i[0]);
+    assign mul2x_o = (pattern_i == 3'b011 || pattern_i == 3'b100);
+    assign mulsign_o = pattern_i[2];
+
 endmodule
