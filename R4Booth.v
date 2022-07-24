9d0e3416219b12fafa1541fe4ad64af388b89afc
hankshyu <hankshyu@gmail.com>
2022-07-25 00:27:00 +0800

    booth part done...

--- R4Booth.v
+++ R4Booth.v
@@ -62,59 +62,36 @@ module R4Booth #(
     wire [PARM_PP - 1 : 0] mulsign; // mulsign_o = bit (i + 1)
     
     
-    genvar j;
     generate
-    for (j = 0; j < 13; j = j+1) begin
-        assign mul1x[j] =  mant_B_Padding[j*2] ^ mant_B_Padding[j*2 + 1];
-        assign mul2x[j] =   ((~mant_B_Padding[j*2]) & (~mant_B_Padding[j*2 + 1]) & (mant_B_Padding[j*2 + 2])) ||
-                            ((mant_B_Padding[j*2]) & (mant_B_Padding[j*2+1]) & (~mant_B_Padding[j*2+2]));
-        assign mulsign[j] = mant_B_Padding[j*2 + 2];
-    end
-    endgenerate
-
-
-
-    wire [PARM_MANT + 2 : 0] mant_AExt = {1'b0,MantA_i,1'b0};
-    wire [PARM_MANT + 1 : 0] booth_PP [PARM_PP - 1: 0]; //Each partial product, except the bottom one, is 1 bits larget(for 2x)
-    
-    reg [PARM_MANT + 1 : 0] booth_PP_new [PARM_PP - 1: 0];
-    wire [PARM_MANT + 1 : 0] booth_PP_new_ans [PARM_PP - 1: 0];
-
-
-
-    generate
-        genvar n,p;
-        for(n = 0; n < 13 ; n = n+1)begin
-            for(p = 0; p < PARM_MANT + 2 ; p = p+1)begin
-                R4Booth_PatternSelector R4Booth_PatternSelector(
-                    .ba_i(mant_AExt[p+1 : p]),
-                    .sel_1x_i(mul1x[n]),
-                    .sel_2x_i(mul2x[n]),
-                    .sel_sign_i(mulsign[n]),
-                    .boothbit_o(booth_PP[n][p])
-                );
-            end
+        genvar j;
+        for (j = 0; j < 13; j = j+1) begin
+            assign mul1x[j] =  mant_B_Padding[j*2] ^ mant_B_Padding[j*2 + 1];
+            assign mul2x[j] =   ((~mant_B_Padding[j*2]) & (~mant_B_Padding[j*2 + 1]) & (mant_B_Padding[j*2 + 2])) ||
+                                ((mant_B_Padding[j*2]) & (mant_B_Padding[j*2+1]) & (~mant_B_Padding[j*2+2]));
+            assign mulsign[j] = mant_B_Padding[j*2 + 2];
         end
     endgenerate
 
+    
+    reg [PARM_MANT + 1 : 0] booth_PP_tmp [PARM_PP - 1: 0];
+    wire [PARM_MANT + 1 : 0] booth_PP [PARM_PP - 1: 0];
+    
     integer idx;
     always @(*) begin
         for (idx = 0; idx < 13; idx = idx + 1) begin
-            if(mul1x[idx]) booth_PP_new[idx] = MantA_i;
-            else if(mul2x[idx]) booth_PP_new[idx] = MantA_i << 1;
-            else booth_PP_new[idx] = 0;
+            if(mul1x[idx]) booth_PP_tmp[idx] = MantA_i;
+            else if(mul2x[idx]) booth_PP_tmp[idx] = MantA_i << 1;
+            else booth_PP_tmp[idx] = 0;
             
         end
     end
 
-    genvar k;
     generate
+        genvar k;
         for(k = 0; k < 13; k = k + 1)begin
-            assign booth_PP_new_ans[k] = (mulsign[k])? ~booth_PP_new[k] : booth_PP_new[k];
+            assign booth_PP[k] = (mulsign[k])? ~booth_PP_tmp[k] : booth_PP_tmp[k];
         end
     endgenerate 
-
-
 
     assign pp_00_o = {21'd0, ~mulsign[ 0],{2{mulsign[0]}},booth_PP[0]}; 
     assign pp_01_o = {21'd1, ~mulsign[ 1], booth_PP[ 1], 1'b0, mulsign[ 0]};
@@ -129,4 +106,5 @@ module R4Booth #(
     assign pp_10_o = { 3'd1, ~mulsign[10], booth_PP[10], 1'b0, mulsign[ 9], 18'd0};
     assign pp_11_o = { 1'd1, ~mulsign[11], booth_PP[11], 1'b0, mulsign[10], 20'd0};
     assign pp_12_o = {booth_PP[12], 1'b0, mulsign[11], 22'd0};
+
 endmodule
