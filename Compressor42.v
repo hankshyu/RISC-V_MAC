b87feb8b4e6eda25203dd700fdc4965638d1c2ba
hankshyu <hankshyu@gmail.com>
2022-07-25 11:01:54 +0800

    42 compressor made

--- Compressor42.v
+++ Compressor42.v
@@ -28,6 +28,32 @@ module Compressor42 #(
     input [XLEN - 1 : 0] C_i,
     input [XLEN - 1 : 0] D_i,
     output [XLEN - 1 : 0] Sum_o,
-    output [XLEN - 1 : 0] Carry_o
+    output [XLEN - 1 : 0] Carry_o,
+    output hidden_carry_msb);
+
+    wire [XLEN - 1: 0] top_sum;
+    wire [XLEN - 1: 0] top_carry;
+
+    Compressor32 top32(
+        .A_i(A_i),
+        .B_i(B_i),
+        .C_i(C_i),
+        .Sum_o(top_sum),
+        .Carry_o(top_carry)
     );
+
+    Compressor32 down32(
+        .A_i(top_sum),
+        .B_i(top_carry),
+        .C_i(D_i),
+        .Sum_o(Sum_o),
+        .Carry_o(Carry_o)
+    );
+
+    assign hidden_carry_msb = top_carry[XLEN - 1];
+
+
+
+
+
 endmodule
--- WallaceTree.v
+++ WallaceTree.v
@@ -41,37 +41,38 @@ module WallaceTree#(
     output [2*PARM_MANT + 2 : 0] wallace_carry_o,
     output suppression_sign_extension_o);
 
-    parameter PARM_CSACOUNT = 10;
 
-    wire  [2*PARM_MANT + 2 : 0] csa_sum [PARM_CSACOUNT - 1: 0];
-    wire  [2*PARM_MANT + 2 : 0] csa_carry [PARM_CSACOUNT - 1: 0];
+    wire  [2*PARM_MANT + 2 : 0] csa_sum [9 - 1: 0];
+    wire  [2*PARM_MANT + 2 : 0] csa_carry [9 - 1: 0];
     
-    wire  [2*PARM_MANT + 2 : 0] csa_shcy [PARM_CSACOUNT - 1: 0];
-    wire  [PARM_CSACOUNT - 1: 0] csa_sig;
+    wire  [2*PARM_MANT + 3 : 0] csa_shcy [9 - 1: 0];
+    wire  [9 : 3] sign_extension;
+
     generate
         genvar j;
-        for(j = 0; j < PARM_CSACOUNT ;j = j+1)begin
+        for(j = 0; j < 8; j = j+1)begin
             assign csa_shcy[j] = csa_carry[j] << 1;
-            assign csa_sig[j] = csa_carry[j][2*PARM_MANT + 2];
         end
     endgenerate
     
     
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV1_0 (.A_i(pp_00_i),.B_i(pp_01_i),.C_i(pp_02_i),.Sum_o(csa_sum[0]),.Carry_o(csa_carry[0]));
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV1_1 (.A_i(pp_03_i),.B_i(pp_04_i),.C_i(pp_05_i),.Sum_o(csa_sum[1]),.Carry_o(csa_carry[1]));
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV1_2 (.A_i(pp_06_i),.B_i(pp_07_i),.C_i(pp_08_i),.Sum_o(csa_sum[2]),.Carry_o(csa_carry[2]));
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV1_3 (.A_i(pp_09_i),.B_i(pp_10_i),.C_i(pp_11_i),.Sum_o(csa_sum[3]),.Carry_o(csa_carry[3]));
+    Compressor32 #(2*PARM_MANT + 3) LV1_0 (.A_i(pp_00_i),.B_i(pp_01_i),.C_i(pp_02_i),.Sum_o(csa_sum[0]),.Carry_o(csa_carry[0]));
+    Compressor32 #(2*PARM_MANT + 3) LV1_1 (.A_i(pp_03_i),.B_i(pp_04_i),.C_i(pp_05_i),.Sum_o(csa_sum[1]),.Carry_o(csa_carry[1]));
+    Compressor32 #(2*PARM_MANT + 3) LV1_2 (.A_i(pp_06_i),.B_i(pp_07_i),.C_i(pp_08_i),.Sum_o(csa_sum[2]),.Carry_o(csa_carry[2]));
+    Compressor32 #(2*PARM_MANT + 3) LV1_3 (.A_i(pp_09_i),.B_i(pp_10_i),.C_i(pp_11_i),.Sum_o(csa_sum[3]),.Carry_o(csa_carry[3]));
+
+    Compressor32 #(2*PARM_MANT + 3) LV2_0 (.A_i(csa_sum[0]),.B_i(csa_shcy[0]),.C_i(csa_sum[1]),.Sum_o(csa_sum[4]),.Carry_o(csa_sum[4]));
+    Compressor32 #(2*PARM_MANT + 3) LV2_1 (.A_i(),.B_i(),.C_i(),.Sum_o(),.Carry_o());
+    Compressor32 #(2*PARM_MANT + 3) LV2_2 (.A_i(),.B_i(),.C_i(),.Sum_o(),.Carry_o());
     
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV2_4 (.A_i(csa_sum[0]),.B_i(csa_shcy[0]),.C_i(csa_sum[1]),.Sum_o(csa_sum[4]),.Carry_o(csa_carry[4]));
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV2_5 (.A_i(csa_shcy[1]),.B_i(csa_sum[2]),.C_i(csa_shcy[2]),.Sum_o(csa_sum[5]),.Carry_o(csa_carry[5]));
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV3_6 (.A_i(csa_sum[3]),.B_i(csa_shcy[3]),.C_i(csa_sum[4]),.Sum_o(csa_sum[6]),.Carry_o(csa_carry[6]));
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV3_7 (.A_i(csa_shcy[4]),.B_i(csa_sum[5]),.C_i(csa_shcy[5]),.Sum_o(csa_sum[7]),.Carry_o(csa_carry[7]));
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV4_8 (.A_i(csa_sum[6]),.B_i(csa_shcy[6]),.C_i(csa_sum[7]),.Sum_o(csa_sum[8]),.Carry_o(csa_carry[8]));
-    CarrySaveAdder #(2*PARM_MANT + 3) csa_LV5_9 (.A_i(csa_shcy[7]),.B_i(csa_shcy[8]),.C_i(csa_sum[8]),.Sum_o(csa_sum[9]),.Carry_o(csa_carry[9]));
+    Compressor32 #(2*PARM_MANT + 3) LV3_0 (.A_i(),.B_i(),.C_i(),.Sum_o(),.Carry_o());
+    Compressor32 #(2*PARM_MANT + 3) LV3_1 (.A_i(),.B_i(),.C_i(),.Sum_o(),.Carry_o());
+    
+    Compressor42 #(2*PARM_MANT + 3) LV4_Final (.A_i(),.B_i(),.C_i(),.D_i().Sum_o(),.Carry_o(),.hidden_carry_msb());
 
     //answer producer
     CarrySaveAdder #(2*PARM_MANT + 3) csa_Final (.A_i(csa_sum[9]),.B_i(csa_shcy[9]),.C_i(pp_12_i),.Sum_o(pp_sum_o),.Carry_o(pp_carry_o)); 
 
-    assign msb_cor_o = | csa_sig[9:3];
+    // assign msb_cor_o = | csa_sig[9:3];
 
 endmodule
