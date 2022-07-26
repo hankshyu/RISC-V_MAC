d8043a20324fb433dc51bb608829f4a696dae89f
hankshyu <hankshyu@gmail.com>
2022-07-26 15:21:01 +0800

    purify prenormalizer

--- MAC32_top.v
+++ MAC32_top.v
@@ -187,19 +187,27 @@ module MAC32_top #(
         .wallace_carry_o(Wallace_carry),
         .suppression_sign_extension_o(Wallace_msb_cor)
     );
-
+    
     //Prenormalization of the augend, in parallel with multiplication.
-    wire [74 : 0] A_Mant_aligned;
-    wire signed [PARM_EXP + 1 : 0] Exp_aligned;
-
-    wire [PARM_EXP + 1 : 0] Exp_mv_neg;
-    wire Mant_sticky_sht_out;
-    wire sign_change_unknown = 1'b0;
     //global signals ...
     wire Sign_aligned;
     wire Exp_mv_sign;
     wire Mv_halt;
+
+    //Exponent Processor
+    wire [PARM_EXP + 1 : 0] Exp_mv = 27 - A_Exp + B_Exp + C_Exp - PARM_BIAS; // d = expA - (expB + expC - 127), mv = 27 - d 
+    wire [PARM_EXP + 1 : 0] Exp_mv_neg = -27 + A_Exp - B_Exp - C_Exp + PARM_BIAS; //Minus_sft_amt_DO
+    assign Exp_mv_sign = Exp_mv[PARM_EXP + 1]; // the sign bit of the mv parameter, Sign_amt_DO
+    assign Mv_halt = (~Exp_mv_sign) & (Exp_mv[PARM_EXP : 0] > 73); //right shift(+) is out of range, which is 74 or more, Sft_stop_SO
+
+    //signals for prenormalizer:
+    wire sign_change_unknown = 1'b0;
     
+    wire [74 : 0] A_Mant_aligned;
+    wire signed [PARM_EXP + 1 : 0] Exp_aligned;
+    wire Mant_sticky_sht_out;
+
+
     PreNormalizer preNormalizer(
         .A_sign_i(A_Sign),
         .B_sign_i(B_Sign),
@@ -210,17 +218,17 @@ module MAC32_top #(
         .C_Exp_i(C_Exp),
         .A_Mant_i(A_Mant),
         .sign_change_i(sign_change_unknown), //this is currently not complete......
+        .Mv_halt_i(Mv_halt),
+        .Exp_mv_i(Exp_mv),
+        .Exp_mv_sign_i(Exp_mv_sign),
 
         .A_Mant_aligned_o(A_Mant_aligned),
         .Exp_aligned_o(Exp_aligned),
         .Sign_aligned_o(Sign_aligned),
-
-        .Exp_mv_sign_o(Exp_mv_sign), //done, Sign_amt_DO
-        .Mv_halt_o(Mv_halt), //, Sft_stop_SO
-
-        .Exp_mv_neg_o(Exp_mv_neg), //done ,Minus_sft_amt_DO
         .Mant_sticky_sht_out_o(Mant_sticky_sht_out)
     );
+
+    //adjust wallace sum to send in... 
     wire [2*PARM_MANT + 2 : 0] Wallace_sum_aligned;
     wire [2*PARM_MANT + 2 : 0] Wallace_carry_aligned;
     
--- PreNormalizer.v
+++ PreNormalizer.v
@@ -34,42 +34,30 @@ module PreNormalizer #(
     input [PARM_EXP - 1 : 0] C_Exp_i,
     input [PARM_MANT : 0] A_Mant_i,
     input sign_change_i, //this is unknown,
+    input Mv_halt_i,
+    input [PARM_EXP + 1 : 0] Exp_mv_i,
+    input Exp_mv_sign_i,
 
-    output [PARM_EXP + 1 : 0] Exp_mv_neg_o, //done ,Minus_sft_amt_DO
-    output Exp_mv_sign_o, //done, Sign_amt_DO
-    output Mv_halt_o, //, Sft_stop_SO
-    
     output Sign_aligned_o,
     output [PARM_EXP + 1: 0] Exp_aligned_o,
     output reg [74 : 0] A_Mant_aligned_o,
-    
-    output reg Mant_sticky_sht_out_o);
-    
-
-    wire [PARM_EXP + 1 : 0] Exp_d;
-    wire [PARM_EXP + 1 : 0] Exp_mv;
-
-    assign Exp_d = A_Exp_i - B_Exp_i - C_Exp_i + PARM_BIAS; // d = expA - (expB + expC - 127)
-    assign Exp_mv = 27 - A_Exp_i + B_Exp_i + C_Exp_i - PARM_BIAS; // mv = 27 - d 
-    assign Exp_mv_neg_o = -27 + A_Exp_i - B_Exp_i - C_Exp_i + PARM_BIAS;
-    assign Exp_mv_sign_o = Exp_mv[PARM_EXP + 1]; // the sign bit of the mv parameter
-
-    assign Mv_halt_o = (~Exp_mv_sign_o) & (Exp_mv[PARM_EXP : 0] > 73); //right shift(+) is out of range, which is 74 or more
+    output reg Mant_sticky_sht_out_o
+    );
     
 
     wire [73 : 0] A_Mant_aligned;
     wire [PARM_MANT : 0] Drop_bits;
-    assign {A_Mant_aligned, Drop_bits} = {A_Mant_i, 74'd0} >> (Mv_halt_o ? 0 : Exp_mv);
+    assign {A_Mant_aligned, Drop_bits} = {A_Mant_i, 74'd0} >> (Mv_halt_i ? 0 : Exp_mv_i);
 
     //output logic for aligner
-    assign Sign_aligned_o = (Exp_mv_sign_o)? A_sign_i : B_sign_i ^ C_sign_i;
-    assign Exp_aligned_o = (Exp_mv_sign_o)? A_Exp_i : (B_Exp_i + C_Exp_i - PARM_BIAS + 27); // exponent = (expB + expC -127) + point distance(= 27)
+    assign Sign_aligned_o = (Exp_mv_sign_i)? A_sign_i : B_sign_i ^ C_sign_i;
+    assign Exp_aligned_o = (Exp_mv_sign_i)? A_Exp_i : (B_Exp_i + C_Exp_i - PARM_BIAS + 27); // exponent = (expB + expC -127) + point distance(= 27)
     
     //output logic for A_Mant_aligned_o
     always @(*) begin
-        if(Exp_mv_sign_o)
+        if(Exp_mv_sign_i)
             A_Mant_aligned_o = (A_Mant_i << 50);
-        else if(~Mv_halt_o)
+        else if(~Mv_halt_i)
             A_Mant_aligned_o = {Sub_Sign_i, {74{Sub_Sign_i}}^A_Mant_aligned};
         else 
             A_Mant_aligned_o = 0;
@@ -81,9 +69,9 @@ module PreNormalizer #(
     //output logic for Mant_sticky_sht_out_o
     always @(*) begin
         if(Sub_Sign_i & (~sign_change_i))
-            Mant_sticky_sht_out_o = (Mv_halt_o)? (|A_Mant_2compelemnt) : (|Drop_bits_2complement);
+            Mant_sticky_sht_out_o = (Mv_halt_i)? (|A_Mant_2compelemnt) : (|Drop_bits_2complement);
         else
-            Mant_sticky_sht_out_o = (Mv_halt_o)? (|A_Mant_i) : (|Drop_bits);
+            Mant_sticky_sht_out_o = (Mv_halt_i)? (|A_Mant_i) : (|Drop_bits);
     end
 
 endmodule
