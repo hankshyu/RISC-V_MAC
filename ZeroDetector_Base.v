74c3ab3d4023e5fe7ff8e78bd6f7c6301ba1970f
hankshyu <hankshyu@gmail.com>
2022-08-14 11:34:08 +0800

    description written, revision info cleared

--- ZeroDetector_Base.v
+++ ZeroDetector_Base.v
@@ -1,22 +1,19 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/29/2022 11:01:59 PM
-// Design Name: 
-// Module Name: ZeroDetector_Base
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Engineer:        Tzu-Han Hsu
+// Create Date:     07/29/2022 11:01:59 PM
+// Module Name:     ZeroDetector_Base
+// Project Name:    IEEE-754 & RISC-V Compatible Multiply-Accumulate Unit
+// HDL(Version):    Verilog-2005
+//
+// Dependencies:    None
+//
+//////////////////////////////////////////////////////////////////////////////////
+// Description:     Check if the input is all zero
+//
+//////////////////////////////////////////////////////////////////////////////////
 // Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
