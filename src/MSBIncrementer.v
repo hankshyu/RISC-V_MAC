1db7796a0b3ac3aba2a4f2d75413041eb5de49fb
hankshyu <hankshyu@gmail.com>
2022-08-15 09:35:06 +0800

    add abandon signal comment

--- src/MSBIncrementer.v
+++ src/MSBIncrementer.v
@@ -27,8 +27,8 @@ module MSBIncrementer #(
     output [PARM_MANT + 3 : 0]  high_sum_o,
     output [PARM_MANT + 3 : 0]  high_sum_inv_o
 );
-    wire high_carry; 
-    wire high_carry_inv;
+    wire high_carry;        // signal that is abandoned
+    wire high_carry_inv;    // signal that is abandoned
     
     assign {high_carry, high_sum_o} = (low_carry_i)? A_Mant_aligned_high_i + 1 : A_Mant_aligned_high_i;
     assign {high_carry_inv, high_sum_inv_o} = (low_carry_inv_i)? ~A_Mant_aligned_high_i : ~A_Mant_aligned_high_i - 1;
