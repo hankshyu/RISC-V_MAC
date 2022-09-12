c15ed92bfbe2523df1c6ac6f8382d69ef783d4a4
hankshyu <hankshyu@gmail.com>
2022-09-12 11:40:05 +0800

    add licence to batch files and improve indention

--- src/ZeroDetector_Group.v
+++ src/ZeroDetector_Group.v
@@ -1,19 +1,61 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Engineer:        Tzu-Han Hsu
-// Create Date:     07/29/2022 11:01:59 PM
-// Module Name:     ZeroDetector_Group
-// Project Name:    IEEE-754 & RISC-V Compatible Multiply-Accumulate Unit
-// HDL(Version):    Verilog-2005
-//
-// Dependencies:    None
-//
-//////////////////////////////////////////////////////////////////////////////////
-// Description:     Check if two groups are all zero
+//  Engineer:        Tzu-Han Hsu
+//  Create Date:     07/29/2022 11:01:59 PM
+//  Module Name:     ZeroDetector_Group
+//  Project Name:    IEEE-754 & RISC-V Compatible Multiply-Accumulate Unit
+//  HDL(Version):    Verilog-2005
+//   
+//  Dependencies:    None
 //
 //////////////////////////////////////////////////////////////////////////////////
-// Revision:
+//  Description:     Check if two groups are all zero
 //
+//////////////////////////////////////////////////////////////////////////////////
+//  Revision:
+//  09/12/2022 - Add BSD-3-Clause Licence
+//   
+//////////////////////////////////////////////////////////////////////////////////
+//  License information:
+//
+//  This software is released under the BSD-3-Clause Licence,
+//  see https://opensource.org/licenses/BSD-3-Clause for details.
+//  In the following license statements, "software" refers to the
+//  "source code" of the complete hardware/software system.
+//
+//  Copyright 2022,
+//                    Embedded Intelligent Systems Lab (EISL)
+//                    Deparment of Computer Science
+//                    National Yang Ming Chiao Tung Uniersity
+//                    Hsinchu, Taiwan.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its contributors
+//     may be used to endorse or promote products derived from this software
+//     without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
 //////////////////////////////////////////////////////////////////////////////////
 
 
